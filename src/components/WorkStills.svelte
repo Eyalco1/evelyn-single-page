8277c0e389132e718cc7ce4143440d226b16e348
Eyal Cohen <eyalcohen008@gmail.com>
2022-11-18 19:34:09 +0200

    Isolated pics data

--- src/components/WorkStills.svelte
+++ src/components/WorkStills.svelte
@@ -1,69 +1,7 @@
 <script>
   import CollagePic from './CollagePic.svelte'
   import StillsSlideshow from './StillsSlideshow.svelte'
-
-  const picsData = [
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/628681824b5d120d3879c3dd_20210902-210902000000010002-2.jpg',
-      alt: '',
-      id: 0
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/628681354b1da92337d1c529_20210831-210831000000030003.jpg',
-      alt: '',
-      id: 1
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/6261686ffd8092a472ccb49c_20210831-210831000000040002.jpg',
-      alt: '',
-      id: 2
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/62616ab245a097f8751d6f97_20210902-210902000000010005.jpg',
-      alt: '',
-      id: 3
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/5ed6451ed9b81a5a0941b792_90353827_251451035883777_2164085926595527432_n.jpg',
-      alt: '',
-      id: 4
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/6266c32416f808767aa5030e_20200317-200317000000050021-2.jpg',
-      alt: '',
-      id: 5
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/5db858647cba6272c868fa09_FFF1.jpg',
-      alt: '',
-      id: 6
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/62616b13cde0428f0da753ac_20210831-210831000000020008.jpg',
-      alt: '',
-      id: 7
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/6266cc38b9fbc454d3536161_20210902-210902000000010003.jpg',
-      alt: '',
-      id: 8
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/5db85b119aa2d737b7ffa5f0_Young%20Fisherman%20-%20Mexico.jpg',
-      alt: '',
-      id: 9
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/5ed644f881818868d908af4c_90348670_570626850328089_727146915985085176_n.jpg',
-      alt: '',
-      id: 10
-    },
-    {
-      src: 'https://uploads-ssl.webflow.com/5d71994f994c3aab41b95759/5ed645bf601842d2ee09d863_30602641_377211976021894_6141786955353948160_n.jpg',
-      alt: '',
-      id: 11
-    }
-  ]
+  import { picsData } from '../data'
 
   let isSlideshowOpen = false
   let activePic = null
